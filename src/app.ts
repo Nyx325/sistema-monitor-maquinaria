aa5e26db98258a8069dcc443f17c7cb4d74c6de9
Dulce Miralrio <mvdo220143@gmail.com>
2024-11-04 11:02:32 -0600

    el login

--- src/app.ts
+++ src/app.ts
@@ -37,4 +37,12 @@ router.get("/equipos", (_request, response) => {
   response.sendFile(path.join(__dirname, "../public/views/equipement.html"));
 });
 
+router.get("/localizacion", (_request, response) => {
+  response.sendFile(path.join(__dirname, "../public/views/localizacion.html"));
+});
+
+router.get("/login", (_request, response) => {
+  response.sendFile(path.join(__dirname, "../public/views/login.html"));
+});
+
 export default router;
